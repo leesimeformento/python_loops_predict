1e2f4e90c7634cf50cec631a25d2d05b60157011
Lee Formento <leesimeformento@gmail.com>
2021-05-06 04:26:38 -0500

    added questionaires

---
+++ python_loops_predict.py
@@ -0,0 +1,71 @@
+# 1
+for i in range(1, 10, 1):
+    print(i)
+# My prediction is (1, 10, 1)
+
+# 2
+for t in range(1, 10, 3):
+    print(t)
+# My Prediction is (1, 10, 3)
+
+
+# 3
+for y in range(5):
+    if y < 5:
+        print(y)
+    elif y == 3:
+        print("y is 3")
+# My Prediction is( 5, 4), (" y is 3")
+
+# 4
+for j in range(20, 1, -3):
+    print(j)
+# My Prediction: (14,11,8,5,2)
+
+# 5
+cities = ["London", "Paris", "Tokyo"]
+for city in cities:
+    print(city)
+# My Prediction: ["London", "Paris", "Tokyo"]
+
+# 6
+numbers = [7, 13, 8, 42]
+for x in range(0, len(numbers)):
+    print(x)
+    print(numbers[x])
+if numbers[len(numbers) - 1] == 42:
+    print("The answer to life, the universe, and everything.")
+# 7
+for num in range(10):
+    if num % 2 == 0:
+        print("Hello")
+    elif num % 4 == 0:
+        print("World")
+    else:
+        print(num)
+# 8
+for num in range(10):
+    if num % 4 == 0:
+        print("Hello")
+    elif num % 2 == 0:
+        print("World")
+    else:
+        print(num)
+# 9
+pet_info = {
+    "name": "Fido",
+    "age": 4,
+    "trick": "rolls over"
+}
+for key in pet_info:
+    print(key)
+    print(pet_info[key])
+# 10
+things_to_remember = {
+    "First": "use the 20 minute rule and use the platform and other resources to find my answer",
+    "Second": "ask my classmates for help, like how I would ask a fellow employee at my job",
+    "Third": "ask an available TA in a public setting, so everyone can benefit from my question",
+    "Fourth": "ask an available instructor"
+}
+for num, step in things_to_remember.items():
+    print(num + ", I will " + step)
